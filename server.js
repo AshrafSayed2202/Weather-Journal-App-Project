aab48b0ad1c9652f80c6abde797e7b2385fd0a69
Ashraf Sayed <ashraf.neizk@gmail.com>
2022-07-20 21:34:07 +0200

    setup

--- server.js
+++ server.js
@@ -2,18 +2,29 @@
 projectData = {};
 
 // Require Express to run server and routes
-
+const express = require('express');
 // Start up an instance of app
-
+const app = express();
 /* Middleware*/
+const bodyParser = require('body-parser');
 //Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
-
+const cors = require('cors');
 // Initialize the main project folder
 app.use(express.static('website'));
 
 
 // Setup Server
+const port = 3000;
+const server = app.listen(port,listening);
+
+function listening(){
+    console.log(`Local server is running on port : ${port}`)
+}
+// GET route setup
+app.get('/weather',function(req,res){
+    res.send(projectData)
+});
--- website/app.js
+++ website/app.js
@@ -1,5 +1,16 @@
-/* Global Variables */
+// Personal API Key for OpenWeatherMap API
+const apiKey = 'd5159581ba8282321c434efef269d6f2&units=imperial';
+const apiUrl= 'https://api.openweathermap.org/data/2.5/weather?&appid='
 
+/* Global Variables */
+const zipCode = document.getElementById('zip');
+const feelings = document.getElementById('feelings').value;
 // Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+let month = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+let newDate = month[d.getMonth()] + '.' +  d.getDate() + '.'+ d.getFullYear();
+
+// Asynchronous function to fetch the data from the app endpoint
+const getData = async function(){
+    
+}
